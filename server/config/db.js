c2a4cb5cf956f2912acdbbdf335746491f0fd372
Sandhurajput <sandhyakumari24@navgurukul.com>
2026-07-24 10:34:22 +0530

    Fix Railway DB port

--- server/config/db.js
+++ server/config/db.js
@@ -5,9 +5,11 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 30000,
 });
 
 db.connect((err) => {
@@ -22,11 +24,7 @@ db.connect((err) => {
 export const executeQuery = (sql, params = []) =>
   new Promise((resolve, reject) => {
     db.execute(sql, params, (err, results) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
+      if (err) return reject(err);
       resolve(results);
     });
   });
