1bd6f741308759179834b489b3af7b7d35cae3c2
Sandhurajput <sandhyakumari24@navgurukul.com>
2026-07-25 21:56:08 +0530

    Debug DB connection

--- server/config/db.js
+++ server/config/db.js
@@ -12,6 +12,14 @@ const db = mysql.createConnection({
   connectTimeout: 30000,
 });
 
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  passwordLength: process.env.DB_PASSWORD?.length,
+});
+
 db.connect((err) => {
   if (err) {
     console.log("Database Connection Failed");
@@ -20,13 +28,4 @@ db.connect((err) => {
     console.log("MySQL Connected Successfully");
   }
 });
-
-export const executeQuery = (sql, params = []) =>
-  new Promise((resolve, reject) => {
-    db.execute(sql, params, (err, results) => {
-      if (err) return reject(err);
-      resolve(results);
-    });
-  });
-
 export default db;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
+import "./config/db.js";
 
 import express from "express";
 import cors from "cors";
+import db from "./config/db.js";
 
 import authRoutes from "./routes/auth.js";
 import teacherRoutes from "./routes/teacher.js";
