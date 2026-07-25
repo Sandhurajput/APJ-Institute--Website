5ba4fe1a6167ca3382a41cdb81629be784ca3c5d
Sandhurajput <sandhyakumari24@navgurukul.com>
2026-07-25 22:13:34 +0530

    chanes

--- server/config/db.js
+++ server/config/db.js
@@ -3,15 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectTimeout: 30000,
-});
-
 console.log({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -20,12 +11,21 @@ console.log({
   passwordLength: process.env.DB_PASSWORD?.length,
 });
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectTimeout: 30000,
+});
+
 db.connect((err) => {
   if (err) {
-    console.log("Database Connection Failed");
     console.log(err);
   } else {
-    console.log("MySQL Connected Successfully");
+    console.log("CONNECTED");
   }
 });
+
 export default db;
